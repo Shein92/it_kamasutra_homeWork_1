f378ec0398bc15e7bb5d41ec28269d880c047509
Shein92 <almashij92@gmail.com>
2020-07-24 09:48:02 +0300

    Updates

---
+++ precache-manifest.18ed677340b01f8ea836e8ae0a93b904.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36766c1f5dc70a003100954d9ba0496",
+    "url": "/it_kamasutra_homeWork_1/index.html"
+  },
+  {
+    "revision": "5bc9633b2634d8b51bad",
+    "url": "/it_kamasutra_homeWork_1/static/css/main.846d0e3c.chunk.css"
+  },
+  {
+    "revision": "01f655d04bafd61fe296",
+    "url": "/it_kamasutra_homeWork_1/static/js/2.7cae7773.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/it_kamasutra_homeWork_1/static/js/2.7cae7773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bc9633b2634d8b51bad",
+    "url": "/it_kamasutra_homeWork_1/static/js/main.e0986f8f.chunk.js"
+  },
+  {
+    "revision": "c91baa6e61a1b192e799",
+    "url": "/it_kamasutra_homeWork_1/static/js/runtime-main.0d8e3a19.js"
+  }
+]);
