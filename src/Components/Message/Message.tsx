9d2573b0ccef6cc7059144510d09103aa81b5af8
Shein92 <almashij92@gmail.com>
2020-07-24 20:22:15 +0300

    smal fixes

--- src/Components/Message/Message.tsx
+++ src/Components/Message/Message.tsx
@@ -14,7 +14,7 @@ function Message(props: MessagePropsType) {
 	return (
 		<div className={mess.message}>
 			<div className={mess.image}>
-				<img src="https://scontent.fudj2-1.fna.fbcdn.net/v/t1.0-9/p960x960/76903549_3488170377922904_1712828928965476352_o.jpg?_nc_cat=101&_nc_sid=85a577&_nc_ohc=eaqtpwSs3JoAX-KLGSy&_nc_ht=scontent.fudj2-1.fna&_nc_tp=6&oh=a4b255e557d7d3e7bcf771b0d8c2318e&oe=5F18BE86" alt="" />
+				<img src="https://scontent.fudj2-1.fna.fbcdn.net/v/t1.0-9/p960x960/76903549_3488170377922904_1712828928965476352_o.jpg?_nc_cat=101&_nc_sid=85a577&_nc_ohc=rfzITQWN4BsAX_Qxja2&_nc_ht=scontent.fudj2-1.fna&_nc_tp=6&oh=77cfa96bb06e31dba12e462119c78e4a&oe=5F404B86" alt="" />
 			</div>
 			<div className={mess.textBuble}>
 				<div className={mess.userName}>
